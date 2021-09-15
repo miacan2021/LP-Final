5d3c700d942f009d455010fb2e478cf2a8fe8407
Shiho <mia.can2021@gmail.com>
2021-09-15 13:17:46 -0700

    added animation and JS scroll function

--- main.js
+++ main.js
@@ -3,22 +3,45 @@ const menuList = document.querySelector('.nav_list');
 const menuItem = document.querySelectorAll('.nav_list_item');
 const wrap = document.querySelector('.wrap');
 
-mobileBtn.addEventListener('click', (() => {
+
+//mobile menu toggle
+function toggle(){
     mobileBtn.classList.toggle('click');
     menuList.classList.toggle('open');
-    console.log(mobileBtn.classList.contains('click'));
+}
+function remove(){
+    mobileBtn.classList.remove('click');
+    menuList.classList.remove('open');
+}
+mobileBtn.addEventListener('click', (() => {
+    toggle();
     if (mobileBtn.classList.contains('click')) {
         wrap.addEventListener('click', (() => {
-            mobileBtn.classList.Fremove('click');
-            menuList.classList.remove('open');
+          remove();
         }))
 
     }
 }));
-
 menuItem.forEach(item => {
     item.addEventListener('click', (() => {
-        mobileBtn.classList.remove('click');
-        menuList.classList.remove('open');
+        remove();
     }));
 })
+
+//scroll event
+
+const items = document.querySelectorAll('.appear');
+
+const active = function(items){
+    items.forEach(item => {
+        if(item.isIntersecting){
+        item.target.classList.add('inview'); 
+        }else{
+            item.target.classList.remove('inview'); 
+        }
+    });
+}
+const io = new IntersectionObserver(active);
+ for(let i=0; i < items.length; i++){
+    io.observe(items[i]);
+ }
