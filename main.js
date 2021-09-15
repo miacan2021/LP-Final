d493abb863a70c865b46923700bb1ce2492a5684
Shiho <mia.can2021@gmail.com>
2021-09-15 08:45:50 -0700

    made a tablet markup

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ mobileBtn.addEventListener('click', (() => {
     mobileBtn.classList.toggle('click');
     menuList.classList.toggle('open');
     console.log(mobileBtn.classList.contains('click'));
-    if(mobileBtn.classList.contains('click')){
-         wrap.addEventListener('click', (() => {
-            mobileBtn.classList.remove('click');
+    if (mobileBtn.classList.contains('click')) {
+        wrap.addEventListener('click', (() => {
+            mobileBtn.classList.Fremove('click');
             menuList.classList.remove('open');
-         }))
-       
+        }))
+
     }
 }));
 
@@ -20,6 +20,5 @@ menuItem.forEach(item => {
     item.addEventListener('click', (() => {
         mobileBtn.classList.remove('click');
         menuList.classList.remove('open');
-    }
-    ));
+    }));
 })
