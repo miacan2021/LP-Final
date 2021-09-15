097185cc3568a856d63469c94b6bee30b52b8d2c
Shiho <mia.can2021@gmail.com>
2021-09-14 22:39:03 -0700

    created footer

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 const mobileBtn = document.querySelector('.berger');
 const menuList = document.querySelector('.nav_list');
 const menuItem = document.querySelectorAll('.nav_list_item');
-
+const body = document.querySelector('body');
 
 mobileBtn.addEventListener('click', (() => {
     mobileBtn.classList.toggle('click');
     menuList.classList.toggle('open');
 }));
+
 menuItem.forEach(item => {
     item.addEventListener('click', (() => {
+        mobileBtn.classList.remove('click');
         menuList.classList.remove('open');
     }
     ));
-    
 })
