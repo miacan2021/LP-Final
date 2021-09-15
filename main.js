c25bdd25929d4b31a05c62ae89e4c77e20ffbdd1
Shiho <mia.can2021@gmail.com>
2021-09-14 19:45:42 -0700

    added apply section

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const mobileBtn = document.querySelector('.berger');
 const menuList = document.querySelector('.nav_list');
-
+const menuItem = document.querySelectorAll('.nav_list_item');
 
 
 mobileBtn.addEventListener('click', (() => {
     mobileBtn.classList.toggle('click');
     menuList.classList.toggle('open');
 }));
-
+menuItem.forEach(item => {
+    item.addEventListener('click', (() => {
+        menuList.classList.remove('open');
+    }
+    ));
+    
+})
