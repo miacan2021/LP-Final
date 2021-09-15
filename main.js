8542008a2300bb657d73d0a2547fa3b5ff7dcb61
Shiho <mia.can2021@gmail.com>
2021-09-14 22:47:10 -0700

    added JS menu toggle function

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const mobileBtn = document.querySelector('.berger');
 const menuList = document.querySelector('.nav_list');
 const menuItem = document.querySelectorAll('.nav_list_item');
-const body = document.querySelector('body');
+const wrap = document.querySelector('.wrap');
 
 mobileBtn.addEventListener('click', (() => {
     mobileBtn.classList.toggle('click');
     menuList.classList.toggle('open');
+    console.log(mobileBtn.classList.contains('click'));
+    if(mobileBtn.classList.contains('click')){
+         wrap.addEventListener('click', (() => {
+            mobileBtn.classList.remove('click');
+            menuList.classList.remove('open');
+         }))
+       
+    }
 }));
 
 menuItem.forEach(item => {
